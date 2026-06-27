b4c151ecf4f7a81913905d9fd6805797a19ff04f
gajanand kumar <gajanandk854@gmail.com>
2026-06-27 22:43:57 +0530

    ci: configure github pages deployment and static export bypass

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repoName = "";
+
+if (isGithubActions) {
+  // Extract repository name from GITHUB_REPOSITORY (e.g., "GAJJUBHAIH/DOWNLODER-PRO")
+  const repo = process.env.GITHUB_REPOSITORY;
+  repoName = repo ? `/${repo.split("/")[1]}` : "";
+}
+
 const nextConfig: NextConfig = {
   // Allow accessing the dev server from phone on the same network
   allowedDevOrigins: ["192.168.31.100"],
+  
+  ...(isGithubActions && {
+    output: "export",
+    basePath: repoName,
+    assetPrefix: repoName,
+  }),
 };
 
 export default nextConfig;
